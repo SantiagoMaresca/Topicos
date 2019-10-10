e80cb9f92215e82c47becec27872b966c258651f
franps <franps.94@hotmail.com>
2019-10-10 20:51:22 -0300

    added oferta component

--- cambioNelson/src/app/app-routing.module.ts
+++ cambioNelson/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PublicacionComponent } from './publicacion/publicacion.component';
-
+import { OfertaComponent } from './oferta/oferta.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'publicacion', component: PublicacionComponent }
+  { path: 'publicacion', component: PublicacionComponent },
+  { path: 'oferta', component: OfertaComponent }
 ];
 
 @NgModule({
--- cambioNelson/src/app/app.module.ts
+++ cambioNelson/src/app/app.module.ts
@@ -7,17 +7,21 @@ import { PublicacionComponent } from './publicacion/publicacion.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { OfertaComponent } from './oferta/oferta.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     PublicacionComponent,
-    HomeComponent
+    HomeComponent,
+    OfertaComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +29,8 @@ import { MatCardModule } from '@angular/material/card';
     NoopAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
