2a4b4ca1c6d10262caf8caaa870b21c877a74436
SantiagoMaresca <santiago.maresca26@gmail.com>
2019-10-17 12:39:28 -0300

    Update .ts
    -

--- cambioNelson/src/app/alta-publicacion/alta-publicacion.component.ts
+++ cambioNelson/src/app/alta-publicacion/alta-publicacion.component.ts
@@ -1,4 +1,10 @@
+import {FormGroup,FormBuilder,Validators} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+
+export interface Divisa {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-alta-publicacion',
@@ -7,9 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AltaPublicacionComponent implements OnInit {
 
-  constructor() { }
+  AltaPublicacionForm: FormGroup;
+  hide = true;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-  }
 
+    this.AltaPublicacionForm= this.formBuilder.group({
+    
+
+    });
+  }
+  PublicarSubmit() {
+    alert(' Nueva publicacion');
+  }
+  divisas: Divisa[] = [
+    {value: 'pesouru-0', viewValue: 'Peso Uruguayo'},
+    {value: 'dolar-1', viewValue: 'Dolar'},
+    {value: 'arg-2', viewValue: 'Peso Argentino'}
+  ];
 }
