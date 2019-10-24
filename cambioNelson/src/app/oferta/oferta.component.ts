7694c251946cb81bf730c6938c9b5c7a64f1fe09
franps <franps.94@hotmail.com>
2019-10-24 19:51:56 -0300

    Offer function working with hardcoded data

--- cambioNelson/src/app/oferta/oferta.component.ts
+++ cambioNelson/src/app/oferta/oferta.component.ts
@@ -6,14 +6,30 @@ import { ServiceService } from '../controller/service.service';
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css']
 })
+
 export class OfertaComponent implements OnInit {
   private publicacion;
   constructor(private service: ServiceService) { }
+
+
 
   ngOnInit() {
     //this.publicacion = this.service.getLastPublicacion();
     this.publicacion = JSON.parse(window.localStorage.getItem("publicacion"));
     console.log(this.publicacion);
   }
+  sendOffer(index) {
+    let data = '{"date":"' + new Date().toLocaleString() + '","quantity":1231231,"badge":"USD","user":"user1@mail.com","publication":"' + this.publicacion['_id'] + '"}';
+    let offer: OfferJSON = JSON.parse(data);
+    let result = this.service.postResource('http://localhost:3000/api/offer', offer);
+  }
 
+}
+
+interface OfferJSON {
+  date: Date;
+  quantity: number;
+  badge: string;
+  user: string;
+  publication: string;
 }
