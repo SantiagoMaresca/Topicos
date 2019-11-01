93160936fc6862851013933cdcc6329aa30a9a25
SantiagoMaresca <santiago.maresca26@gmail.com>
2019-11-01 15:55:17 -0300

    Update filter.pipe.ts
    -

--- cambioNelson/src/app/publicaciones/filter.pipe.ts
+++ cambioNelson/src/app/publicaciones/filter.pipe.ts
@@ -8,7 +8,9 @@ export class FilterPipe implements PipeTransform {
   transform(value: any, searchValue): any {
     console.log(searchValue.user);
     if (searchValue=='') return value;
-    return value.filter((v) => v.user.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 || v.badge.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
+    return value.filter((v) => v.user.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 || v.badge.toLowerCase().indexOf(searchValue.toLowerCase()) > -1
+    || v.date.toLowerCase().indexOf(searchValue.toLowerCase()) > -1|| v.place.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 
+    || v.quantity.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
 
   }
 
